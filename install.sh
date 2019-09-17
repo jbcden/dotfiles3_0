7694e8f7465ebd698952191e2c65fa2839db2a31
Jacob Chae <jbcden@gmail.com>
2019-09-17 21:48:41 +0900

    Add some more neovim setup to install script
    More needs done here -- including adding auto-installing all plugins

--- install.sh
+++ install.sh
@@ -17,6 +17,16 @@ done
 mkdir -p ~/.config/nvim
 ln -s $PWD/init.vim ~/.config/nvim/init.vim
 
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+else
+  git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
+fi
+
+# Dependency for deoplete autocomplete plugin
+pip3 install --user pynvim
+
 # if [ ! -e "$target" ]; then
 #   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # fi
