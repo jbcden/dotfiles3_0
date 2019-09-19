3b83c4cdc1139d253ea59a7ca32597494c65aee7
YOUR_NAME <YOUR_EMAIL>
2019-09-19 14:33:13 +0900

    rough draft

--- install.sh
+++ install.sh
@@ -14,28 +14,4 @@ for name in $(ls configs/); do
   fi
 done
 
-mkdir -p ~/.config/nvim
-ln -s $PWD/init.vim ~/.config/nvim/init.vim
-
-if ! [ -x "$(command -v git)" ]; then
-  echo 'Error: git is not installed.' >&2
-  exit 1
-else
-  if ! [ -e ~/.vim/pack/minpac/opt/minpac ]; then
-    git clone https://github.com/k-takata/minpac.git ~/.vim/pack/minpac/opt/minpac
-  fi
-fi
-
-# Dependency for deoplete autocomplete plugin
-if [ -x "$(command -v pip3)" ]; then
-  pip3 install --user pynvim
-else
-  echo "pip is not installed, vim deoplete will not be usable until this is fixed"
-fi
-
 mkdir -p ~/.psql
-
-# if [ ! -e "$target" ]; then
-#   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# fi
-# vim -u ~/.vimrc +BundleInstall +qa
