0074fc85786f574c9258c74ab72eee48bbc783cd
Jacob Chae <jbcden@gmail.com>
2019-12-05 10:14:13 +0900

    The android-sdk package is now a cask

--- mac_package_installer.sh
+++ mac_package_installer.sh
@@ -1,6 +1,6 @@
 brew_taps=("bsiegel/tap" "caskroom/cask" "goles/battery" "simeji/jid")
-packages=("neovim" "android-sdk" "libsigsegv" "openssl" "vim" "autoconf" "cscope" "freetype" "jid" "libssh" "optipng" "rbenv" "watch" "automake" "ctags" "fswatch" "gnutls" "jpeg" "sqlite" "wget" "gcc" "jpegoptim" "libtiff" "parallel" "rbenv-gemset" "battery" "dlite" "jq" "libtool" "msgpack" "pcre" "readline" "the_silver_searcher" "xhyve" "haproxy" "libxml2" "perl" "reattach-to-user-namespace" "gettext" "libxslt" "redis" "tmux" "libevent" "libyaml" "nmap" "pidcat" "ripgrep" "tree" "ghostscript" "libffi" "ruby" "git" "hub" "llvm" "noti" "pkg-config" "ruby-build" "glib" "imagemagick" "libpcl" "python" "ruby-install" "coreutils" "libpng" "lua" "openssh" "qt" "bat" "postgres" "kubectl")
-casks=("chromedriver")
+packages=("neovim" "libsigsegv" "openssl" "vim" "autoconf" "cscope" "freetype" "jid" "libssh" "optipng" "rbenv" "watch" "automake" "ctags" "fswatch" "gnutls" "jpeg" "sqlite" "wget" "gcc" "jpegoptim" "libtiff" "parallel" "rbenv-gemset" "battery" "dlite" "jq" "libtool" "msgpack" "pcre" "readline" "the_silver_searcher" "xhyve" "haproxy" "libxml2" "perl" "reattach-to-user-namespace" "gettext" "libxslt" "redis" "tmux" "libevent" "libyaml" "nmap" "pidcat" "ripgrep" "tree" "ghostscript" "libffi" "ruby" "git" "hub" "llvm" "noti" "pkg-config" "ruby-build" "glib" "imagemagick" "libpcl" "python" "ruby-install" "coreutils" "libpng" "lua" "openssh" "qt" "bat" "postgres" "kubectl")
+casks=("chromedriver" "android-sdk")
 
 if hash brew 2>/dev/null; then
   echo "brew is already install"
